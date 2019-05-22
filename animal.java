8867083b5767372f71ce01b466786738a228dfb4
Olegrybka <orybka2488@gmail.com>
2019-05-22 20:55:49 +0300

    Подготовил ДЗ к уроку 6

---
+++ animal.java
@@ -0,0 +1,9 @@
+abstract class animal {
+
+    abstract void run (int length);
+
+    abstract void swim (int length);
+
+    abstract void jump (int height);
+
+}
---
+++ main.java
@@ -0,0 +1,14 @@
+public class main {
+    public static void main(String[] args) {
+        cat cat1 = new cat();
+        cat1.run(200);
+        cat1.swim(1);
+        cat1.jump(2);
+
+        dog dog1 = new dog();
+        dog1.run(500);
+        dog1.swim(10);
+        dog1.jump(0.5);
+
+    }
+}
